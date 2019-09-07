11469796f787e4caff37d028f55295b3ad0aa75d
Kip Kipper <kip@kipper.me>
2019-09-07 11:02:02 -0700

    Some color utilities

--- Kelpie/Kelpie/ViewControllers/HomeViewController.swift
+++ Kelpie/Kelpie/ViewControllers/HomeViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Elliott Kipper on 9/3/19.
 //  Copyright © 2019 Kip. All rights reserved.
 //
-
+import Hue
 import UIKit
 
 class HomeViewController: UIViewController {
@@ -19,6 +19,9 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let bg = [UIColor.kelpieMint, UIColor.kelpieMint.darker].gradient()
+        bg.frame = self.view.bounds
+        self.view.layer.insertSublayer(bg, at: 0)
     }
 
 }
--- Kelpie/Kelpie/Views/ViewUtilities.swift
+++ Kelpie/Kelpie/Views/ViewUtilities.swift
@@ -151,3 +151,23 @@ extension UIImage {
         return newImage
     }
 }
+
+
+extension UIColor {
+    
+    var darker: UIColor {
+        var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+        if self.getRed(&r, green: &g, blue: &b, alpha: &a){
+            return UIColor(red: max(r - 0.2, 0.0), green: max(g - 0.2, 0.0), blue: max(b - 0.2, 0.0), alpha: a)
+        }
+        return self
+    }
+    var lighter: UIColor {
+        var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+        if self.getRed(&r, green: &g, blue: &b, alpha: &a){
+            return UIColor(red: min(r + 0.2, 1.0), green: min(g + 0.2, 1.0), blue: min(b + 0.2, 1.0), alpha: a)
+        }
+        return self
+    }
+    
+}
